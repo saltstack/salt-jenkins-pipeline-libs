2ead962550f406cdbc133a5645a1ebb5ed8a399b
Pedro Algarvio <pedro@algarvio.me>
2020-04-10 07:45:05 +0100

    Packer will login to Docker HUB for us

--- vars/withDockerHubCredentials.groovy
+++ vars/withDockerHubCredentials.groovy
@@ -1,22 +1,13 @@
 
 def call(String credentials_id, Closure body=null) {
 
-    try {
-        withCredentials([
-            usernamePassword(
-                credentialsId: credentials_id,
-                passwordVariable: 'DOCKER_PASSWORD',
-                usernameVariable: 'DOCKER_USERNAME')
-        ]) {
-            sh """
-            echo \${DOCKER_PASSWORD} | docker login --username \${DOCKER_USERNAME} --password-stdin https://index.docker.io/v1/
-            """
-        }
+    withCredentials([
+        usernamePassword(
+            credentialsId: credentials_id,
+            passwordVariable: 'DOCKER_PASSWORD',
+            usernameVariable: 'DOCKER_USERNAME')
+    ]) {
         if (body) { body() }
-    } finally {
-        sh """
-        docker logout https://index.docker.io/v1/
-        """
     }
 }
 // vim: ft=groovy ts=4 sts=4 et
