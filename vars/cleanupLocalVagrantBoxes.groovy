2d84614c006b678ebef9889779e3f62b18b9dd60
Bryce Larson <brycel@vmware.com>
2021-07-27 17:27:26 +0000

    bump up vm_cleanup_seconds on mac

--- vars/cleanupLocalVagrantBoxes.groovy
+++ vars/cleanupLocalVagrantBoxes.groovy
@@ -3,7 +3,7 @@ def call(Map options) {
     def Integer global_timeout = options.get('global_timeout', 8)
 
     stage('Cleanup Local Vagrant Boxes') {
-        vm_cleanup_seconds = global_timeout * 3600 + 3600
+        vm_cleanup_seconds = global_timeout * 3600 + 10800
         sh """
         # Only remove running VMs here so we don't interfere with building golden images.
         # Stopped VMs will be cleaned up by a jenkins startup script when it connects.
