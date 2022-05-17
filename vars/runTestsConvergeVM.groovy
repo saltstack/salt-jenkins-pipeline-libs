dbb2927656431c761624873e9c6d83dabb807aa5
Pedro Algarvio <palgarvio@vmware.com>
2022-05-17 11:44:59 +0100

    Return status codes
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- vars/runTestsConvergeVM.groovy
+++ vars/runTestsConvergeVM.groovy
@@ -8,25 +8,36 @@ def call(String converge_stage_name,
          String distro_name,
          String test_suite_name_slug) {
 
+    def Integer returnStatus = 1;
+
     stage(converge_stage_name) {
-        if ( macos_build ) {
-            withEnv(["MACOS_PYTHON_VERSION=${macos_python_version}"]) {
+        try {
+            if ( macos_build ) {
+                withEnv(["MACOS_PYTHON_VERSION=${macos_python_version}"]) {
+                    sh '''
+                    ssh-agent /bin/bash -xc 'ssh-add ~/.vagrant.d/insecure_private_key; bundle exec kitchen converge $TEST_SUITE-$TEST_PLATFORM; (exitcode=$?; echo "ExitCode: $exitcode"; exit $exitcode);'
+                    '''
+                }
+            } else {
                 sh '''
-                ssh-agent /bin/bash -xc 'ssh-add ~/.vagrant.d/insecure_private_key; bundle exec kitchen converge $TEST_SUITE-$TEST_PLATFORM; (exitcode=$?; echo "ExitCode: $exitcode"; exit $exitcode);'
+                ssh-agent /bin/bash -xc 'ssh-add ~/.ssh/kitchen.pem; bundle exec kitchen converge $TEST_SUITE-$TEST_PLATFORM; (exitcode=$?; echo "ExitCode: $exitcode"; exit $exitcode);'
                 '''
             }
-        } else {
-            sh '''
-            ssh-agent /bin/bash -xc 'ssh-add ~/.ssh/kitchen.pem; bundle exec kitchen converge $TEST_SUITE-$TEST_PLATFORM; (exitcode=$?; echo "ExitCode: $exitcode"; exit $exitcode);'
-            '''
+            sh """
+            if [ -s ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ]; then
+                mv ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-converge.log"
+            fi
+            if [ -s ".kitchen/logs/kitchen.log" ]; then
+                mv ".kitchen/logs/kitchen.log" ".kitchen/logs/kitchen-${test_suite_name_slug}-converge.log"
+            fi
+            """
+            returnStatus = 0
+        } finally {
+            archiveArtifacts(
+                artifacts: ".kitchen/logs/*-converge.log",
+                allowEmptyArchive: true
+            )
+            return returnStatus
         }
-        sh """
-        if [ -s ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ]; then
-            mv ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-converge.log"
-        fi
-        if [ -s ".kitchen/logs/kitchen.log" ]; then
-            mv ".kitchen/logs/kitchen.log" ".kitchen/logs/kitchen-${test_suite_name_slug}-converge.log"
-        fi
-        """
     }
 }
