84f41750e4a9f07e88432e41769a74976837ac82
Pedro Algarvio <palgarvio@vmware.com>
2022-05-19 10:00:23 +0100

    Rework the run tests part
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

---
+++ vars/runTestsChunk.groovy
@@ -0,0 +1,204 @@
+
+def call(String nox_passthrough_opts,
+         String test_paths,
+         String chunk_name,
+         String python_version,
+         String distro_version,
+         String distro_arch,
+         String distro_name,
+         String test_suite_name_slug,
+         Boolean upload_test_coverage,
+         Boolean upload_split_test_coverage) {
+
+    def Integer returnStatus = 1;
+
+    def local_environ
+
+    try {
+        local_environ = [
+            "NOX_PASSTHROUGH_OPTS=${nox_passthrough_opts} ${test_paths}"
+        ]
+        try {
+            withEnv(local_environ) {
+                withEnv(["DONT_DOWNLOAD_ARTEFACTS=1"]) {
+                    returnStatus = sh(
+                        returnStatus: true,
+                        label: "Run ${chunk_name.capitalize()} Tests",
+                        script: 'bundle exec kitchen verify $TEST_SUITE-$TEST_PLATFORM; (exitcode=$?; echo "ExitCode: $exitcode"; exit $exitcode);'
+                    )
+                }
+            }
+        } finally {
+            sh """
+            if [ -s ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ]; then
+                mv ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-${chunk_name}-verify.log"
+            fi
+            if [ -s ".kitchen/logs/kitchen.log" ]; then
+                mv ".kitchen/logs/kitchen.log" ".kitchen/logs/kitchen-${test_suite_name_slug}-${chunk_name}-verify.log"
+            fi
+            """
+
+            // Let's report about known problems found
+            def List<String> conditions_found = []
+            reportKnownProblems(conditions_found, ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-${chunk_name}-verify.log")
+
+            withEnv(["ONLY_DOWNLOAD_ARTEFACTS=1"]){
+                sh 'bundle exec kitchen verify $TEST_SUITE-$TEST_PLATFORM || exit 0'
+            }
+            sh """
+            if [ -s ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ]; then
+                mv ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-${chunk_name}-download.log"
+            fi
+            if [ -s ".kitchen/logs/kitchen.log" ]; then
+                mv ".kitchen/logs/kitchen.log" ".kitchen/logs/kitchen-${test_suite_name_slug}-${chunk_name}-download.log"
+            fi
+            if [ -s "artifacts/coverage/.coverage" ]; then
+                mv artifacts/coverage/.coverage artifacts/coverage/.coverage-${chunk_name}
+            fi
+            if [ -s "artifacts/coverage/salt.xml" ]; then
+                mv artifacts/coverage/salt.xml artifacts/coverage/salt-${chunk_name}.xml
+            fi
+            if [ -s "artifacts/coverage/tests.xml" ]; then
+                mv artifacts/coverage/tests.xml artifacts/coverage/tests-${chunk_name}.xml
+            fi
+            """
+            sh """
+            # Do not error if there are no files to compress
+            xz .kitchen/logs/*-verify.log || true
+            # Do not error if there are no files to compress
+            xz artifacts/logs/runtests-*.log || true
+            """
+            if ( upload_test_coverage == true ) {
+                def distro_strings = [
+                    distro_name,
+                    distro_version,
+                    distro_arch
+                ]
+                def report_strings = (
+                    [python_version] + nox_env_name.split('-') + [chunk_name]
+                ).flatten()
+                if ( upload_split_test_coverage ) {
+                    uploadCodeCoverage(
+                        report_path: "artifacts/coverage/tests-${chunk_name}.xml",
+                        report_name: "${distro_strings.join('-')}-${report_strings.join('-')}-tests",
+                        report_flags: ([distro_strings.join('')] + report_strings + ['tests']).flatten()
+                    )
+                    uploadCodeCoverage(
+                        report_path: "artifacts/coverage/salt-${chunk_name}.xml",
+                        report_name: "${distro_strings.join('-')}-${report_strings.join('-')}-salt",
+                        report_flags: ([distro_strings.join('')] + report_strings + ['salt']).flatten()
+                    )
+                } else {
+                    uploadCodeCoverage(
+                        report_path: "artifacts/coverage/salt-${chunk_name}.xml",
+                        report_name: "${distro_strings.join('-')}-${report_strings.join('-')}-salt",
+                        report_flags: ([distro_strings.join('')] + report_strings).flatten()
+                    )
+                }
+            }
+            archiveArtifacts(
+                artifacts: ".kitchen/logs/*-verify.log*,.kitchen/logs/*-download.log",
+                allowEmptyArchive: true
+            )
+        }
+        if ( returnStatus != 0 ) {
+            local_environ = [
+                "NOX_PASSTHROUGH_OPTS=${nox_passthrough_opts} ${test_paths} --lf"
+            ]
+            try {
+                withEnv(local_environ) {
+                    withEnv(["DONT_DOWNLOAD_ARTEFACTS=1"]) {
+                        returnStatus = sh(
+                            returnStatus: true,
+                            label: "Re-Run ${chunk_name.capitalize()} Tests",
+                            script: 'bundle exec kitchen verify $TEST_SUITE-$TEST_PLATFORM; (exitcode=$?; echo "ExitCode: $exitcode"; exit $exitcode);'
+                        )
+                    }
+                }
+            } finally {
+                sh """
+                if [ -s ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ]; then
+                    mv ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-${chunk_name}-rerun-verify.log"
+                fi
+                if [ -s ".kitchen/logs/kitchen.log" ]; then
+                    mv ".kitchen/logs/kitchen.log" ".kitchen/logs/kitchen-${test_suite_name_slug}-${chunk_name}-rerun-verify.log"
+                fi
+                """
+
+                // Let's report about known problems found
+                def List<String> conditions_found_rerun = []
+                reportKnownProblems(conditions_found_rerun, ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-${chunk_name}-rerun-verify.log")
+
+                withEnv(["ONLY_DOWNLOAD_ARTEFACTS=1"]){
+                    sh 'bundle exec kitchen verify $TEST_SUITE-$TEST_PLATFORM || exit 0'
+                }
+                sh """
+                if [ -s ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ]; then
+                    mv ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-${chunk_name}-rerun-download.log"
+                fi
+                if [ -s ".kitchen/logs/kitchen.log" ]; then
+                    mv ".kitchen/logs/kitchen.log" ".kitchen/logs/kitchen-${test_suite_name_slug}-${chunk_name}-rerun-download.log"
+                fi
+                if [ -s "artifacts/coverage/.coverage" ]; then
+                    mv artifacts/coverage/.coverage artifacts/coverage/.coverage-rerun-${chunk_name}
+                fi
+                if [ -s "artifacts/coverage/salt.xml" ]; then
+                    mv artifacts/coverage/salt.xml artifacts/coverage/salt-rerun-${chunk_name}.xml
+                fi
+                if [ -s "artifacts/coverage/tests.xml" ]; then
+                    mv artifacts/coverage/tests.xml artifacts/coverage/tests-rerun-${chunk_name}.xml
+                fi
+                """
+                sh """
+                # Do not error if there are no files to compress
+                xz .kitchen/logs/*-verify.log || true
+                # Do not error if there are no files to compress
+                xz artifacts/logs/runtests-*.log || true
+                """
+                if ( upload_test_coverage == true ) {
+                    def distro_strings = [
+                        distro_name,
+                        distro_version,
+                        distro_arch
+                    ]
+                    def report_strings = (
+                        [python_version] + nox_env_name.split('-') + [chunk_name]
+                    ).flatten()
+                    if ( upload_split_test_coverage ) {
+                        uploadCodeCoverage(
+                            report_path: "artifacts/coverage/tests-rerun-${chunk_name}.xml",
+                            report_name: "${distro_strings.join('-')}-${report_strings.join('-')}-rerun-tests",
+                            report_flags: ([distro_strings.join('')] + report_strings + ['tests']).flatten()
+                        )
+                        uploadCodeCoverage(
+                            report_path: "artifacts/coverage/salt-rerun-${chunk_name}.xml",
+                            report_name: "${distro_strings.join('-')}-${report_strings.join('-')}-rerun-salt",
+                            report_flags: ([distro_strings.join('')] + report_strings + ['salt']).flatten()
+                        )
+                    } else {
+                        uploadCodeCoverage(
+                            report_path: "artifacts/coverage/salt-rerun-${chunk_name}.xml",
+                            report_name: "${distro_strings.join('-')}-${report_strings.join('-')}-rerun-salt",
+                            report_flags: ([distro_strings.join('')] + report_strings).flatten()
+                        )
+                    }
+                }
+                archiveArtifacts(
+                    artifacts: ".kitchen/logs/*-verify.log*,.kitchen/logs/*-download.log",
+                    allowEmptyArchive: true
+                )
+            }
+        }
+    } finally {
+        archiveArtifacts(
+            artifacts: "artifacts/*,artifacts/**/*,artifacts/xml-unittests-output/*.xml",
+            allowEmptyArchive: true
+        )
+        junit(
+            keepLongStdio: true,
+            skipPublishingChecks: true,
+            testResults: 'artifacts/xml-unittests-output/*.xml'
+        )
+        return returnStatus
+    }
+}
---
+++ vars/runTestsFull.groovy
@@ -0,0 +1,131 @@
+
+def call(String run_tests_stage_name,
+         String nox_passthrough_opts,
+         String python_version,
+         String distro_version,
+         String distro_arch,
+         String distro_name,
+         String test_suite_name_slug,
+         Integer inactivity_timeout_minutes,
+         Boolean run_full,
+         Boolean upload_test_coverage,
+         Boolean upload_split_test_coverage) {
+
+    def Integer returnStatus = 0;
+    def Integer chunkReturnStatus = 0;
+
+    def cause
+    def String timeout_id
+    def String timeout_message
+    def String original_run_tests_stage
+    def local_environ
+    def List<String> test_paths
+    def String chunk_name
+    def List<String> ignore_paths = []
+
+    stage(run_tests_stage_name) {
+        try {
+            timeout(activity: true, time: inactivity_timeout_minutes, unit: 'MINUTES') {
+                chunk_name = "unit"
+                test_paths = ["tests/unit", "tests/pytests/unit"]
+                test_paths.each { path ->
+                    ignore_paths << "--ignore=${path}"
+                }
+                chunkReturnStatus = runTestsChunk(
+                    nox_passthrough_opts,
+                    test_paths.join(" "),
+                    chunk_name,
+                    python_version,
+                    distro_version,
+                    distro_arch,
+                    distro_name,
+                    test_suite_name_slug,
+                    upload_test_coverage,
+                    upload_split_test_coverage
+                )
+                returnStatus = returnStatus + chunkReturnStatus
+                if ( chunkReturnStatus != 0 && run_full == false ) {
+                    error("Failed to run ${chunk_name} tests")
+                }
+                chunk_name = "functional"
+                test_paths = ["tests/pytests/functional"]
+                test_paths.each { path ->
+                    ignore_paths << "--ignore=${path}"
+                }
+                chunkReturnStatus = runTestsChunk(
+                    nox_passthrough_opts,
+                    test_paths.join(" "),
+                    chunk_name,
+                    python_version,
+                    distro_version,
+                    distro_arch,
+                    distro_name,
+                    test_suite_name_slug,
+                    upload_test_coverage,
+                    upload_split_test_coverage
+                )
+                returnStatus = returnStatus + chunkReturnStatus
+                if ( chunkReturnStatus != 0 && run_full == false ) {
+                    error("Failed to run ${chunk_name} tests")
+                }
+                chunk_name = "scenarios"
+                test_paths = ["tests/pytests/scenarios"]
+                test_paths.each { path ->
+                    ignore_paths << "--ignore=${path}"
+                }
+                chunkReturnStatus = runTestsChunk(
+                    nox_passthrough_opts,
+                    test_paths.join(" "),
+                    chunk_name,
+                    python_version,
+                    distro_version,
+                    distro_arch,
+                    distro_name,
+                    test_suite_name_slug,
+                    upload_test_coverage,
+                    upload_split_test_coverage
+                )
+                returnStatus = returnStatus + chunkReturnStatus
+                if ( chunkReturnStatus != 0 && run_full == false ) {
+                    error("Failed to run ${chunk_name} tests")
+                }
+                chunk_name = "integration"
+                test_paths = ignore_paths
+                chunkReturnStatus = runTestsChunk(
+                    nox_passthrough_opts,
+                    test_paths.join(" "),
+                    chunk_name,
+                    python_version,
+                    distro_version,
+                    distro_arch,
+                    distro_name,
+                    test_suite_name_slug,
+                    upload_test_coverage,
+                    upload_split_test_coverage
+                )
+                returnStatus = returnStatus + chunkReturnStatus
+                if ( chunkReturnStatus != 0 && run_full == false ) {
+                    error("Failed to run ${chunk_name} tests")
+                }
+            }
+        } catch(org.jenkinsci.plugins.workflow.steps.FlowInterruptedException inactivity_exception) { // timeout reached
+            cause = inactivity_exception.causes.get(0)
+            if (cause instanceof org.jenkinsci.plugins.workflow.steps.TimeoutStepExecution.ExceededTimeout) {
+                timeout_id = "inactivity-timeout"
+                timeout_message = "No output was seen for ${inactivity_timeout_minutes} minutes. Aborted ${run_tests_stage_name}."
+                addWarningBadge(
+                    id: timeout_id,
+                    text: timeout_message
+                )
+                createSummary(
+                    id: timeout_id,
+                    icon: 'warning.png',
+                    text: "<b>${timeout_message}</b>"
+                )
+            }
+            throw inactivity_exception
+        } finally {
+            return returnStatus
+        }
+    }
+}
