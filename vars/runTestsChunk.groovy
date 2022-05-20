dbd3bab0ad5eb9aec99bbd2f80e500895b55fd70
Pedro Algarvio <palgarvio@vmware.com>
2022-05-20 13:32:31 +0100

    Nested into the finally block?!
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- vars/runTestsChunk.groovy
+++ vars/runTestsChunk.groovy
@@ -114,107 +114,108 @@ def call(String nox_passthrough_opts,
                 sh """
                 rm .kitchen/logs/*-verify.log* .kitchen/logs/*-download.log* artifacts/logs/${chunk_name}-runtests.log* || true
                 """
-            }
-        }
-        if ( returnStatus != 0 ) {
-            deleteRemoteArtifactsDir()
-            local_environ = [
-                "NOX_PASSTHROUGH_OPTS=${nox_passthrough_opts} ${test_paths} --lf"
-            ]
-            try {
-                withEnv(local_environ) {
-                    withEnv(["DONT_DOWNLOAD_ARTEFACTS=1"]) {
-                        sh(
-                            label: "Re-Run ${chunk_name.capitalize()} Tests",
-                            script: 'bundle exec kitchen verify $TEST_SUITE-$TEST_PLATFORM; (exitcode=$?; echo "ExitCode: $exitcode"; exit $exitcode);'
-                        )
-                    }
-                }
-                returnStatus = 0
-            } catch (rerun_error) {
-                echo "Failed to re-run ${chunk_name.capitalize()} Tests ${run_type}."
-                returnStatus = 1
-                throw rerun_error
-            } finally {
-                sh """
-                if [ -s ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ]; then
-                    mv ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-${chunk_name}-rerun-verify.log"
-                fi
-                if [ -s ".kitchen/logs/kitchen.log" ]; then
-                    mv ".kitchen/logs/kitchen.log" ".kitchen/logs/kitchen-${test_suite_name_slug}-${chunk_name}-rerun-verify.log"
-                fi
-                """
 
-                // Let's report about known problems found
-                def List<String> conditions_found_rerun = []
-                reportKnownProblems(conditions_found_rerun, ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-${chunk_name}-rerun-verify.log")
-
-                withEnv(["ONLY_DOWNLOAD_ARTEFACTS=1"]){
-                    sh 'bundle exec kitchen verify $TEST_SUITE-$TEST_PLATFORM || exit 0'
-                }
-                sh """
-                if [ -s ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ]; then
-                    mv ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-${chunk_name}-rerun-download.log"
-                fi
-                if [ -s ".kitchen/logs/kitchen.log" ]; then
-                    mv ".kitchen/logs/kitchen.log" ".kitchen/logs/kitchen-${test_suite_name_slug}-${chunk_name}-rerun-download.log"
-                fi
-                if [ -s "artifacts/coverage/.coverage" ]; then
-                    mv artifacts/coverage/.coverage artifacts/coverage/.coverage-rerun-${chunk_name}
-                fi
-                if [ -s "artifacts/coverage/salt.xml" ]; then
-                    mv artifacts/coverage/salt.xml artifacts/coverage/salt-rerun-${chunk_name}.xml
-                fi
-                if [ -s "artifacts/coverage/tests.xml" ]; then
-                    mv artifacts/coverage/tests.xml artifacts/coverage/tests-rerun-${chunk_name}.xml
-                fi
-                mv artifacts/logs/\$(ls artifacts/logs/ | sort -r | head -n 1) artifacts/logs/${chunk_name}-rerun-runtests.log || true
-                rm artifacts/logs/runtests-*.log || true
-                mv artifacts/xml-unittests-output/\$(ls artifacts/xml-unittests-output | sort -r | head -n 1) artifacts/xml-unittests-output/${chunk_name}-rerun-test-results.xml || true
-                rm artifacts/xml-unittests-output/test-results-*.xml || true
-                """
-                sh """
-                # Do not error if there are no files to compress
-                xz .kitchen/logs/*-verify.log || true
-                # Do not error if there are no files to compress
-                xz artifacts/logs/${chunk_name}-rerun-runtests.log || true
-                """
-                if ( upload_test_coverage == true ) {
-                    def distro_strings = [
-                        distro_name,
-                        distro_version,
-                        distro_arch
+                if ( returnStatus != 0 ) {
+                    deleteRemoteArtifactsDir()
+                    local_environ = [
+                        "NOX_PASSTHROUGH_OPTS=${nox_passthrough_opts} ${test_paths} --lf"
                     ]
-                    def report_strings = (
-                        [python_version] + nox_env_name.split('-') + [chunk_name]
-                    ).flatten()
-                    if ( upload_split_test_coverage ) {
-                        uploadCodeCoverage(
-                            report_path: "artifacts/coverage/tests-rerun-${chunk_name}.xml",
-                            report_name: "${distro_strings.join('-')}-${report_strings.join('-')}-rerun-tests",
-                            report_flags: ([distro_strings.join('')] + report_strings + ['tests']).flatten()
+                    try {
+                        withEnv(local_environ) {
+                            withEnv(["DONT_DOWNLOAD_ARTEFACTS=1"]) {
+                                sh(
+                                    label: "Re-Run ${chunk_name.capitalize()} Tests",
+                                    script: 'bundle exec kitchen verify $TEST_SUITE-$TEST_PLATFORM; (exitcode=$?; echo "ExitCode: $exitcode"; exit $exitcode);'
+                                )
+                            }
+                        }
+                        returnStatus = 0
+                    } catch (rerun_error) {
+                        echo "Failed to re-run ${chunk_name.capitalize()} Tests ${run_type}."
+                        returnStatus = 1
+                        throw rerun_error
+                    } finally {
+                        sh """
+                        if [ -s ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ]; then
+                            mv ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-${chunk_name}-rerun-verify.log"
+                        fi
+                        if [ -s ".kitchen/logs/kitchen.log" ]; then
+                            mv ".kitchen/logs/kitchen.log" ".kitchen/logs/kitchen-${test_suite_name_slug}-${chunk_name}-rerun-verify.log"
+                        fi
+                        """
+
+                        // Let's report about known problems found
+                        def List<String> conditions_found_rerun = []
+                        reportKnownProblems(conditions_found_rerun, ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-${chunk_name}-rerun-verify.log")
+
+                        withEnv(["ONLY_DOWNLOAD_ARTEFACTS=1"]){
+                            sh 'bundle exec kitchen verify $TEST_SUITE-$TEST_PLATFORM || exit 0'
+                        }
+                        sh """
+                        if [ -s ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ]; then
+                            mv ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}.log" ".kitchen/logs/${python_version}-${distro_name}-${distro_version}-${distro_arch}-${test_suite_name_slug}-${chunk_name}-rerun-download.log"
+                        fi
+                        if [ -s ".kitchen/logs/kitchen.log" ]; then
+                            mv ".kitchen/logs/kitchen.log" ".kitchen/logs/kitchen-${test_suite_name_slug}-${chunk_name}-rerun-download.log"
+                        fi
+                        if [ -s "artifacts/coverage/.coverage" ]; then
+                            mv artifacts/coverage/.coverage artifacts/coverage/.coverage-rerun-${chunk_name}
+                        fi
+                        if [ -s "artifacts/coverage/salt.xml" ]; then
+                            mv artifacts/coverage/salt.xml artifacts/coverage/salt-rerun-${chunk_name}.xml
+                        fi
+                        if [ -s "artifacts/coverage/tests.xml" ]; then
+                            mv artifacts/coverage/tests.xml artifacts/coverage/tests-rerun-${chunk_name}.xml
+                        fi
+                        mv artifacts/logs/\$(ls artifacts/logs/ | sort -r | head -n 1) artifacts/logs/${chunk_name}-rerun-runtests.log || true
+                        rm artifacts/logs/runtests-*.log || true
+                        mv artifacts/xml-unittests-output/\$(ls artifacts/xml-unittests-output | sort -r | head -n 1) artifacts/xml-unittests-output/${chunk_name}-rerun-test-results.xml || true
+                        rm artifacts/xml-unittests-output/test-results-*.xml || true
+                        """
+                        sh """
+                        # Do not error if there are no files to compress
+                        xz .kitchen/logs/*-verify.log || true
+                        # Do not error if there are no files to compress
+                        xz artifacts/logs/${chunk_name}-rerun-runtests.log || true
+                        """
+                        if ( upload_test_coverage == true ) {
+                            def distro_strings = [
+                                distro_name,
+                                distro_version,
+                                distro_arch
+                            ]
+                            def report_strings = (
+                                [python_version] + nox_env_name.split('-') + [chunk_name]
+                            ).flatten()
+                            if ( upload_split_test_coverage ) {
+                                uploadCodeCoverage(
+                                    report_path: "artifacts/coverage/tests-rerun-${chunk_name}.xml",
+                                    report_name: "${distro_strings.join('-')}-${report_strings.join('-')}-rerun-tests",
+                                    report_flags: ([distro_strings.join('')] + report_strings + ['tests']).flatten()
+                                )
+                                uploadCodeCoverage(
+                                    report_path: "artifacts/coverage/salt-rerun-${chunk_name}.xml",
+                                    report_name: "${distro_strings.join('-')}-${report_strings.join('-')}-rerun-salt",
+                                    report_flags: ([distro_strings.join('')] + report_strings + ['salt']).flatten()
+                                )
+                            } else {
+                                uploadCodeCoverage(
+                                    report_path: "artifacts/coverage/salt-rerun-${chunk_name}.xml",
+                                    report_name: "${distro_strings.join('-')}-${report_strings.join('-')}-rerun-salt",
+                                    report_flags: ([distro_strings.join('')] + report_strings).flatten()
+                                )
+                            }
+                        }
+                        archiveArtifacts(
+                            artifacts: ".kitchen/logs/*-verify.log*,.kitchen/logs/*-download.log,artifacts/logs/${chunk_name}-rerun-runtests.log*",
+                            allowEmptyArchive: true
                         )
-                        uploadCodeCoverage(
-                            report_path: "artifacts/coverage/salt-rerun-${chunk_name}.xml",
-                            report_name: "${distro_strings.join('-')}-${report_strings.join('-')}-rerun-salt",
-                            report_flags: ([distro_strings.join('')] + report_strings + ['salt']).flatten()
-                        )
-                    } else {
-                        uploadCodeCoverage(
-                            report_path: "artifacts/coverage/salt-rerun-${chunk_name}.xml",
-                            report_name: "${distro_strings.join('-')}-${report_strings.join('-')}-rerun-salt",
-                            report_flags: ([distro_strings.join('')] + report_strings).flatten()
-                        )
+                        // Once archived, delete
+                        sh """
+                        rm .kitchen/logs/*-verify.log* .kitchen/logs/*-download.log* artifacts/logs/${chunk_name}-rerun-runtests.log* || true
+                        """
                     }
                 }
-                archiveArtifacts(
-                    artifacts: ".kitchen/logs/*-verify.log*,.kitchen/logs/*-download.log,artifacts/logs/${chunk_name}-rerun-runtests.log*",
-                    allowEmptyArchive: true
-                )
-                // Once archived, delete
-                sh """
-                rm .kitchen/logs/*-verify.log* .kitchen/logs/*-download.log* artifacts/logs/${chunk_name}-rerun-runtests.log* || true
-                """
             }
         } finally {
             try {
