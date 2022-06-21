9ddbd06e35fb681b2b3c4f28db18af38128b708c
Pedro Algarvio <palgarvio@vmware.com>
2022-06-21 18:21:15 +0100

    Don't use ``sudo`` and default to the ``python3`` binary
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- vars/deleteRemoteArtifactsDir.groovy
+++ vars/deleteRemoteArtifactsDir.groovy
@@ -2,7 +2,7 @@
 def call() {
     try {
         sh '''
-        bundle exec kitchen exec $TEST_SUITE-$TEST_PLATFORM -c "sudo python -c \\"import shutil; shutil.rmtree('artifacts', ignore_errors=True);\\""
+        bundle exec kitchen exec $TEST_SUITE-$TEST_PLATFORM -c "python3 -c \\"import shutil; shutil.rmtree('artifacts', ignore_errors=True);\\""
         '''
     } catch (Exception error) {
         echo "Failed to delete remote artifacts directory: ${error}"
